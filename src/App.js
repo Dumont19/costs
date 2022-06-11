ee0e2b1992950c958cc9e639ea1052c42d069006
Matheus Dumont <95390248+Dumont19@users.noreply.github.com>
2022-06-11 01:13:06 -0300

    Layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,41 @@
+import React from 'react'
+
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+
+import Home from './components/pages/Home'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import NewProject from './components/pages/NewProject'
+
+import Container from './components/layout/Container'
+
 function App() {
   return (
-    <div>Hello world!</div>
+    <Router>
+      <div>
+        <Link to='/'>Home</Link>
+        <Link to='/Contact'>Contato</Link>
+        <Link to='/Company'>Empresa</Link>
+        <Link to='/NewProject'>Novo Projeto</Link>
+      </div>
+      <Switch>
+      <Container customClass='min_height'>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/Contact'>
+            <Contact />
+          </Route>
+          <Route path='/Company'>
+            <Company />
+          </Route>
+          <Route path='/NewProject'>
+            <NewProject />
+          </Route>
+        </Container>
+      </Switch>
+      <p>Footer</p>
+    </Router>
   )
 }
 
