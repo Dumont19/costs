83fafa034c933cc2c17f5951bc80231500cd08e5
Matheus Dumont <95390248+Dumont19@users.noreply.github.com>
2022-06-16 03:20:54 -0300

    Layouts components and home page

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
 import React from 'react'
-
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import Home from './components/pages/Home'
 import Company from './components/pages/Company'
 import Contact from './components/pages/Contact'
 import NewProject from './components/pages/NewProject'
+import Projects from './components/pages/Projects'
 
 import Container from './components/layout/Container'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to='/'>Home</Link>
-        <Link to='/Contact'>Contato</Link>
-        <Link to='/Company'>Empresa</Link>
-        <Link to='/NewProject'>Novo Projeto</Link>
-      </div>
+      <Navbar />
       <Switch>
-      <Container customClass='min_height'>
-          <Route exact path='/'>
-            <Home />
-          </Route>
-          <Route path='/Contact'>
-            <Contact />
-          </Route>
-          <Route path='/Company'>
-            <Company />
-          </Route>
-          <Route path='/NewProject'>
-            <NewProject />
-          </Route>
+        <Container customClass='min_height'>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route path='/Projects'>
+              <Projects />
+            </Route>
+            <Route path='/Contact'>
+              <Contact />
+            </Route>
+            <Route path='/Company'>
+              <Company />
+            </Route>
+            <Route path='/NewProject'>
+              <NewProject />
+            </Route>
         </Container>
       </Switch>
-      <p>Footer</p>
+      <Footer />
     </Router>
   )
 }
